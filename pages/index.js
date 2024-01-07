782ec11c5e274a166eff300d13fe47bfe1b620e1
shirinf <sforouzan@my.bcit.ca>
2024-01-06 16:17:01 -0800

    updated version

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,16 @@ export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        Twitter
+        <div className={styles.largecontainer}>
+          <div className={styles.logocontainer}>
+        <Image className={styles.logoimage} src={'/images/xlogo.svg'} alt="x logo" width={100} height={100}/>
+          </div>
+          
+          <div className={styles.assetscontainer}>
+          <h1 className={styles.texth1}>Happening now</h1>
+          <h3 className={styles.texth3}>Join today.</h3>
+          </div>
+        </div> 
       </main>
     </>
   )
