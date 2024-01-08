36c9bbc65bf40b0389ca2eda1fac3165e069e933
shirinf <sforouzan@my.bcit.ca>
2024-01-07 17:23:39 -0800

    updated twitter page

---
+++ components/Button1/index.js
@@ -0,0 +1,8 @@
+import styles from './Button1.module.css'
+export default function Button1() {
+    return(
+        <button className={styles.button1}>
+            Sign up with Google
+        </button>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import Button1 from '@/components/Button1'
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>X. It's what's happening / X</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
       <main className={styles.main}>
         <div className={styles.largecontainer}>
           <div className={styles.logocontainer}>
@@ -14,6 +20,7 @@ export default function Home() {
           <div className={styles.assetscontainer}>
           <h1 className={styles.texth1}>Happening now</h1>
           <h3 className={styles.texth3}>Join today.</h3>
+          <Button1 />
           </div>
         </div> 
       </main>
