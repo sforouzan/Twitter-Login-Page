c121ea1120c5d9fe41f1126c265ba93de2bf73c7
shirinf <sforouzan@my.bcit.ca>
2024-01-07 20:04:52 -0800

    all buttons and footer added

---
+++ components/Button4/index.js
@@ -0,0 +1,8 @@
+import styles from './Button4.module.css'
+export default function Button4() {
+    return(
+        <button className={styles.button4}>
+            Sign in
+        </button>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '@/styles/Home.module.css'
 import Button1 from '@/components/Button1'
 import Button2 from '@/components/Button2'
 import Button3 from '@/components/Button3'
+import Button4 from '@/components/Button4'
 
 export default function Home() {
   return (
@@ -32,8 +33,38 @@ export default function Home() {
           </div>
         </div>
         <Button3 />
+        <div className={styles.textcontainer}>        
+          <p className={styles.signuptext}>
+          By signing up, you agree to the <span className={styles.bluetext}>Terms of Service</span> and <span className={styles.bluetext}>Privacy Policy</span>, including <span className={styles.bluetext}>Cookie Use.</span>
+          </p>
+          </div>
+        <h5 className={styles.texth5}>Already have an account?</h5>
+        <Button4 />
           </div>
         </div> 
+        <footer className={styles.footer}>
+        <ul>
+          <li>About</li>
+          <li>Download the X app</li>
+          <li>Help Center</li>
+          <li>Terms of Service</li>
+          <li>Privacy Policy</li>
+          <li>Cookie Policy</li>
+          <li>Accessibility</li>
+          <li>Ads info</li>
+          <li>Blog</li>
+          <li>Status</li>
+          <li>Careers</li>
+          <li>Brand Resources</li>
+          <li>Advertising</li>
+          <li>Marketing</li>
+          <li>X for Business</li>
+          <li>Developers</li>
+          <li>Directory</li>
+          <li>Settings</li>
+          <li>© 2024 X Corp.</li>
+        </ul>
+      </footer>
       </main>
     </>
   )
